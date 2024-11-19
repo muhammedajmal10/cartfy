a3f77bcc328b32692037b2c4b60960fd3baed5fe
Muhammed Ajmal <119556276+muhammedajmal10@users.noreply.github.com>
2024-11-20 00:32:54 +0530

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,49 @@
+import imageProduct1 from './assets/images/sofa1.png' 
+import imageProduct2 from './assets/images/sofa2.png' 
+import imageProduct3 from './assets/images/sofa3.png' 
+import imageProduct4 from './assets/images/sofa4.png' 
+import imageProduct5 from './assets/images/sofa 5.png' 
+
+export const products = [
+    {
+        id: 1,
+        name: 'Hertfoid Upholstered Chair',
+        price: 101,
+        image: imageProduct1,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Abingdon Upholstered Chair Swivel',
+        price: 151,
+        image: imageProduct2,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Jeses Minimore Modern Style Etta',
+        price: 200,
+        image: imageProduct3,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Supported model chair',
+        price: 261,
+        image: imageProduct4,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Wooden stylish Arm chair',
+        price: 250,
+        image: imageProduct5,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+ 
+];
